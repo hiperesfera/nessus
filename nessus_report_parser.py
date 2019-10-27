1055a197fdbca3640cc772c6da7202147c4e184d
Jesus Rodriguez Fonteboa <hiperesfera@gmail.com>
2019-10-27 22:43:00 +0000

    Update nessus_report_parser.py

--- nessus_report_parser.py
+++ nessus_report_parser.py
@@ -2,30 +2,51 @@
 # sc.scan_instances.export_scan(scan_id), file_object)
 # the result of above method is a ZIP file containg the Nessus report in SCAP (XML) format
 
-import xmltodict
+
 import xml.etree.ElementTree as ET
 
+    
+    
+root = ET.parse('./842.nessus_v2').getroot()
 
-with open('./842.nessus') as fd:
-    doc = xmltodict.parse(fd.read())
-        
-root = ET.parse('./842.nessus').getroot()
+nessus_dict = {}
+nessus_matrix = []
 
-for tag in root.findall('Report/ReportHost'):
-    machine_name = tag.get('name')
-    tags = root.findall('Report/ReportHost/HostProperties/tag')
-    os = [i.text for i in tags if i.get('name') == 'operating-system']
-    ip = [i.text for i in tags if i.get('name') == 'host-ip']
-    netbios = [i.text for i in tags if i.get('name') == 'netbios-name']
-    for tag in root.findall('Report/ReportHost/ReportItem'):
-        pluginID = tag.get('pluginID')
-        severity = int(tag.get('severity'))
-        pluginName = tag.get('pluginName')  #tab is a dictionary so you can use tag['severity'] 
-        description = tag.find('description').text
-        synopsis = tag.find('synopsis').text 
-        cve = tag.find('cve').text if tag.find('cve') is not None else None
-        cvss = tag.find('cvss3_base_score').text if tag.find('cvss3_base_score') is not None else None   
-        if severity >= 2:
-            print(machine_name,"\t",netbios,"\t",os,"\t",ip,"\t",pluginID,"\t",severity,"\t",pluginName,"\t",cve,"\t",cvss,)
-            print(synopsis)
+
+for host in root.findall('Report/ReportHost'):
+    
+    machine_name = host.get('name')
+    nessus_dict = { machine_name:{} } 
+    
+    
+    for tag in host.find('HostProperties').iter('tag'):
+        if tag.attrib['name'] == 'operating-system':
+            os = tag.text 
+            print(os)
+        if tag.attrib['name'] == 'netbios-name':
+            netbios = tag.text 
+            print(netbios)
+        if tag.attrib['name'] == 'host-ip':
+            ip = tag.text 
+            print(ip)
+            
+    
+    for reportItem in host.iter('ReportItem'):
+        port = int(reportItem.get('port'))
+        pluginId = int(reportItem.get('pluginID'))
+        pluginName = reportItem.get('pluginName')
+        synopsis = reportItem.get('synopsis')
+        severity = int(reportItem.get('severity'))
+        cve = tag.find('cve').text if tag.find('cve') is not None else None
+        cvss = tag.find('cvss3_base_score').text if tag.find('cvss3_base_score') is not None else None 
+        
+        if severity >= 2:
+            nessus_matrix.append([machine_name,port,netbios,os,ip,pluginId,severity,pluginName,cve,cvss,synopsis])
+            nessus_dict[machine_name][port]= { 'netbios':netbios,'ip':ip,'os':os,'pluginId':pluginId,'pluginName':pluginName,'severity':severity,'cve':cve,'cvss':cvss,'synopsis':synopsis }
+            
+            print(machine_name,"\t",port,"\t",netbios,"\t",os,"\t",ip,"\t",pluginId,"\t",severity,"\t",pluginName,"\t",cve,"\t",cvss,'\t',synopsis)
+        
+    
+    print("----------------------------------")
+    
            
